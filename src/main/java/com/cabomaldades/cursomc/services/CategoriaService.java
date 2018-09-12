aba280aa22be20ed5e0dc601a29b76e9b945be01
Cabomaldades <soutom@yahoo.com.br>
2018-09-11 22:06:28 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/cabomaldades/cursomc/services/CategoriaService.java
+++ src/main/java/com/cabomaldades/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cabomaldades.cursomc.domain.Categoria;
+import com.cabomaldades.cursomc.dto.CategoriaDTO;
 import com.cabomaldades.cursomc.repositories.CategoriaRepository;
 import com.cabomaldades.cursomc.services.exceptions.DataIntegrityException;
 import com.cabomaldades.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,4 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
+	}
+	
 }
